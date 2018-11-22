3ed02a4a4c785b4c5d788655dd03ae9cff22945f
jungleknife <42376221+jungleknife@users.noreply.github.com>
2018-11-22 09:21:40 +0000

    Add files via upload

---
+++ PrescriptionSln/PrescriptionItem/PrescriptionItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrescriptionItemSln
+{
+    public class PrescriptionItem
+    {
+        public PrescriptionItem()
+        {
+
+        }
+        public string PharmaceuticalName { get; private set; }
+        public int PrescribedDailyDose { get; set; }
+        public int Duration { get; set; }
+        public int ContainerSize { get; private set; }
+        public bool AvailableOverTheCounter { get; private set; }
+        public string Comments { get; set; }
+        public int NumberOfContainers { get; set; }
+
+    }
+}
---
+++ PrescriptionSln/PrescriptionSln/Prescription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrescriptionItemSln;
+
+namespace PrescriptionSln
+{
+    public class Prescription
+    {
+        public void AddPrescriptionItem(string name, int dosage, int duration, 
+            int containerSize, bool overCounter, string comments)
+        {
+
+        }
+        public void RemovePrescriptionItem(string name)
+        {
+
+        }
+        public void ClearPrescription()
+        {
+
+        }
+        public PrescriptionItem PrescriptionItems { get; set; }
+        public int NumberOfPharmaceuticals { get; private set; }
+        public int NumberOfContainers { get; private set; }
+    }
+}
